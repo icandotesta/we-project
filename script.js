5b243ff760e7990c63ca76ea52ee19b70fca45c3
Ibrahim Yusif <yibrahim021@st.ug.edu.gh>
2024-12-28 22:13:34 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//   const balanceNow=document.querySelector(".balance");
+//  balanceNow.textContent=110;
+const balanceNow=document.querySelector(".balance");
+balanceNow.textContent=20000;
+const expenses=[10, 30, 23,56]
+console.log(expenses.length);
