e247c404e9dda3922cb1833b31c511efc5ccaf01
Ibrahim Yusif <yibrahim021@st.ug.edu.gh>
2025-01-02 21:35:55 -0800

    updated

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-//   const balanceNow=document.querySelector(".balance");
-//  balanceNow.textContent=110;
-const balanceNow=document.querySelector(".balance");
-balanceNow.textContent=20000;
-const expenses=[10, 30, 23,56]
-console.log(expenses.length);
+
+// const salary=2400
+
+// const balance=document.querySelector('.balance');
+// balance.textContent=salary;
+// const transaction={
+//     description:salary,
+//     amount:500,
+// }
+// const person={
+//     _name:"kofi",
+//     age:10,
+//     interest:'football',
+
+// }
+
+// console.log(transaction.amount);
+// console.log(person._name ,person.age,person.interest,);
+
+function  transactionBalannce(){
+    const totalAmount=50;
+    const transaction=20;
+    const balance=totalAmount-transaction;
+    return balance
+
+}
+const result=transactionBalannce();
+console.log(result)
+
+
+const trying = (a,b)=> {
+    console.log("see me", a,b)
+}
+trying(100,200)
